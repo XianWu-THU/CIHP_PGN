890c3d4c9e9cd4f85274c1ea14642e160431a9f0
XianWu <thuwx95@gmail.com>
2020-05-01 01:07:23 +0800

    test fashion

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@ from .utils import decode_labels, inv_preprocess, prepare_label, save, load
 from .ops import conv2d, max_pool, linear
 from .image_reader import ImageReader
 from .image_reader_pgn import ImageReaderPGN
+from .base_reader import BaseReader
